48e8b93a4b6a0bff692538482c4dcb26ddd6c237
FlashGod123 <67604319+FlashGod123@users.noreply.github.com>
2021-10-31 15:50:13 -0700

    Add files via upload

--- page.py
+++ page.py
@@ -1,19 +1,35 @@
+import tkinter as tk 
 from tkinter import *
+from tkinter import messagebox
 
 def mainScreen():
     main_screen = Tk()
     main_screen.title("F.R.E.S")
     main_screen.mainloop()
 
-#choose to see message or scan face
-Label(text="Face Recognition Emotional Support",bg = "orange",width="300", height="2").pack()
-Label(text="").pack()
-Label(text="").pack()
-Label(text="").pack()
+class Buttons():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("250x100")
+        
+        Label(text="Face Recognition Emotional Support",bg = "orange",width="300", height="2").pack()
+        Label(text="").pack()
+        Label(text="").pack()
+        self.butt1 = Button(self.root,
+                                text="Scan",
+                                height="2",
+                                width="30",
+                                command=''
+                                )
+  
+        self.butt2 = Button(self.root,text="Message",height="2",width="30")
 
-Button(text="Scan",height="2",width="30").pack()
-Label(text="").pack()
-
-Button(text="Message",height="2",width="30").pack()
-
+        self.butt1.pack(side=tk.TOP)
+        Label(text="").pack()
+        self.butt2.pack(side=tk.TOP)
+        self.root.mainloop()
+    def showInfo(self):
+        self.butt1['command'] = messagebox.showinfo( "Look at the Camera", "Press q to stop scanning")
+        
+test = Buttons()
 mainScreen()
