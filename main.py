fbdd1fca20b74ad46f9bfeca4658fcbbd7fe98cf
Fupeng Zhao <72841750+ConnerZhao@users.noreply.github.com>
2021-10-31 15:46:14 -0700

    update

---
+++ main.py
@@ -0,0 +1,82 @@
+
+from tkinter import *
+import cv2
+import numpy as np
+from keras.preprocessing import image
+import warnings
+warnings.filterwarnings("ignore")
+from keras.preprocessing.image import load_img, img_to_array 
+from keras.models import  load_model
+import matplotlib.pyplot as plt
+import numpy as np
+import tkinter
+
+#window
+tkWindow = Tk()  
+tkWindow.geometry('400x150')  
+tkWindow.title('Tkinter Login Form - pythonexamples.org')
+
+# load model
+model = load_model("best_model.h5")
+
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+
+
+f = open("emotions.txt", "r")
+
+def scanButton():
+    while True:
+        f = open("emotions.txt", "w")
+        ret, test_img = cap.read()  # captures frame and returns boolean value and captured image
+        if not ret:
+            continue
+        gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+
+        faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+        for (x, y, w, h) in faces_detected:
+            cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=6)
+            roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
+            roi_gray = cv2.resize(roi_gray, (224, 224))
+            img_pixels = image.img_to_array(roi_gray)
+            img_pixels = np.expand_dims(img_pixels, axis=0)
+            img_pixels /= 255
+
+            predictions = model.predict(img_pixels)
+
+            # find max indexed arra y
+            max_index = np.argmax(predictions[0])
+
+            emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+            predicted_emotion = emotions[max_index]
+            f.write(emotions[max_index] + "\n")
+            # manipulate this code here, modify to our need
+            # have it output in variables, do if emotion == "one of the options"
+
+
+            cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        resized_img = cv2.resize(test_img, (1000, 700))
+        cv2.imshow('Facial emotion analysis ', resized_img)
+        if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+            f.close()
+            break
+
+    cap.release()
+    cv2.destroyAllWindows
+
+
+quitButton = tkinter.Button(tkWindow, 
+                   text="Quit", 
+                   fg="red",
+                   command=quit)
+
+#login button
+scan = Button(tkWindow, text="Scan", command = scanButton)
+scan.pack()
+quitButton.pack()
+f.close()
+tkWindow.mainloop()
--- videotester.py
+++ videotester.py
@@ -1,4 +1,3 @@
-import os
 import cv2
 import numpy as np
 from keras.preprocessing import image
@@ -12,13 +11,12 @@ import numpy as np
 # load model
 model = load_model("best_model.h5")
 
-
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
 
 cap = cv2.VideoCapture(0)
 
 while True:
+    f = open("emotions.txt", "w")
     ret, test_img = cap.read()  # captures frame and returns boolean value and captured image
     if not ret:
         continue
@@ -27,7 +25,7 @@ while True:
     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
     for (x, y, w, h) in faces_detected:
-        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=6)
         roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
         roi_gray = cv2.resize(roi_gray, (224, 224))
         img_pixels = image.img_to_array(roi_gray)
@@ -36,18 +34,22 @@ while True:
 
         predictions = model.predict(img_pixels)
 
-        # find max indexed array
+        # find max indexed arra y
         max_index = np.argmax(predictions[0])
 
         emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
         predicted_emotion = emotions[max_index]
+        f.write(emotions[max_index] + "\n")
+        # manipulate this code here, modify to our need
+        # have it output in variables, do if emotion == "one of the options"
+
 
         cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     resized_img = cv2.resize(test_img, (1000, 700))
     cv2.imshow('Facial emotion analysis ', resized_img)
-
     if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+        f.close()
         break
 
 cap.release()
